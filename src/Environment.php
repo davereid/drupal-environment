3ce094ce89c5331c5c296eeb84932c02717b005f
Dave Reid <dave@davereid.net>
2025-08-29 10:08:58 -0500

    Fix PHPCS violations.

--- src/Environment.php
+++ src/Environment.php
@@ -193,19 +193,21 @@ class Environment
         if (!isset($host)) {
             $possibleHostSources = array('HTTP_X_FORWARDED_HOST', 'HTTP_HOST', 'SERVER_NAME', 'SERVER_ADDR');
             $sourceTransformations = array(
-                "HTTP_X_FORWARDED_HOST" => function($value) {
+                "HTTP_X_FORWARDED_HOST" => function ($value) {
                     $elements = explode(',', $value);
                     return trim(end($elements));
                 }
             );
             $host = '';
-            foreach ($possibleHostSources as $source)
-            {
-                if (!empty($host)) break;
-                if (empty($_SERVER[$source])) continue;
+            foreach ($possibleHostSources as $source) {
+                if (!empty($host)) {
+                    break;
+                }
+                if (empty($_SERVER[$source])) {
+                    continue;
+                }
                 $host = $_SERVER[$source];
-                if (array_key_exists($source, $sourceTransformations))
-                {
+                if (array_key_exists($source, $sourceTransformations)) {
                     $host = $sourceTransformations[$source]($host);
                 }
             }
